d18b94df8b649ce1703d2b702dec515b8995f377
paulaali <paulawakabi@gmail.com>
2023-04-25 11:42:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = sizeof(message) - 1;
-    ssize_t ret = write(2, message, len);
-    if (ret != len) {
-        return 1;
-    }
-    return 1;
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
-
