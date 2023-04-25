0b5b7bf376f7b9ea2adb5a84523c77999554d067
paulaali <paulawakabi@gmail.com>
2023-04-25 09:37:29 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,6 @@ int main(void)
 	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("size of a float: %zu byte(s)\n", sizeof(float));
+	fprintf(stderr, "[Anything]");
 	return (0);
 }
