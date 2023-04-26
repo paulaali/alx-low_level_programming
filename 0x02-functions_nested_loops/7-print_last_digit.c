9854879b31e01f0674fae28b4d4ec9f0f16ab46f
paulaali <paulawakabi@gmail.com>
2023-04-26 11:19:34 -0700

    7-last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,26 @@
 #include "main.h"
-
 /**
- * main - check the code
- *@n the number
- * Return: Always 0.
+ * _abs- returns 1 if n is lowercase, 0 otherwise
+ * @n: integer to be tested
+ * Return: abs(n)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-1 * n);
+}
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: integer whose last digit is to be printed
+ * Return: last digit of n
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n *= -1;
-	 int ld = n % 10;
-	return (ld);
+	int lastDigit;
+
+	lastDigit = _abs((n % 10));
+	_putchar(lastDigit + 48);
+	return (lastDigit);
 }
