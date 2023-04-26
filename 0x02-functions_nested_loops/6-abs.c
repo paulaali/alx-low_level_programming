a5b37bf3ba4e0e5cee93f3d708c450084493e2b4
paulaali <paulawakabi@gmail.com>
2023-04-25 19:04:52 -0700

    task6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,21 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
+ * _abs - compute the absolute value of an intiger
+ *@t: the value to be inputed
  * Return: Always 0.
  */
-int main(void)
+int _abs(int t)
 {
-int r;
-
-r = _abs(-1);
-printf("%d\n", r);
-r = _abs(0);
-printf("%d\n", r);
-r = _abs(1);
-printf("%d\n", r);
-r = _abs(-98);
-printf("%d\n", r);
-return (0);
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
 }
